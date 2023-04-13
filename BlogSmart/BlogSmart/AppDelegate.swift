edde126e06fbe20583c5640548f4b31dd4acf908
Raunaq Malhotra <raunaqmalhotra02@gmail.com>
2023-04-13 15:18:52 -0400

    set up the Xcode project

---
+++ BlogSmart/BlogSmart/AppDelegate.swift
@@ -0,0 +1,87 @@
+//
+//  AppDelegate.swift
+//  BlogSmart
+//
+//  Created by Raunaq Malhotra on 4/13/23.
+//
+
+import UIKit
+import ParseSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        ParseSwift.initialize(applicationId: "bYiJVZ0HsxO4Un39geLjnlEeVXGhjHjJ5KBj1ZRV",
+                              clientKey: "ienfPnKOFY7fHcaG4anVvys3qGIcwaPnlQbIVl0W",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+        
+        //Test code to check if Parse is working fine
+        var score = GameScore(playerName: "ChrisEvans", points: 398)
+        
+        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+        // If no callbackQueue is specified it returns to main queue.
+        score.save { result in
+            switch result {
+            case .success(let savedScore):
+                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+            case .failure(let error):
+                assertionFailure("Error saving: \(error)")
+            }
+        }
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+// Test Code
+ 
+// Create your own value type 'ParseObject'.
+struct GameScore: ParseObject {
+    // Required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    // All custom properties must be optional.
+    var playerName: String?
+    var points: Int?
+}
+
+// Implement a custom initializer (OPTIONAL i.e. NOT REQUIRED)
+// It's recommended to place custom initializers in an extension
+// to preserve the memberwise initializer.
+extension GameScore {
+    init(playerName: String, points: Int) {
+        self.playerName = playerName
+        self.points = points
+    }
+}
+
+// Sample Usage
+//
+// let score = GameScore(playerName: "Kingsley", points: 13)
+//
