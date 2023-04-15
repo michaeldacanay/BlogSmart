8b3b818852dd8337d4d37e0d5ee3adedb73da85e
edake1 <ed433@cornell.edu>
2023-04-14 23:38:07 -0400

    Added Models folder and created User file

---
+++ BlogSmart/BlogSmart/Models/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  BlogSmart
+//
+//  Created by Edwin Dake on 4/14/23.
+//
+
+import Foundation
+
+import ParseSwift
+
+struct User: ParseUser {
+    // These are required by `ParseObject`.
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // These are required by `ParseUser`.
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String: [String: String]?]?
+
+//    var lastPostedDate: Date?
+
+}
