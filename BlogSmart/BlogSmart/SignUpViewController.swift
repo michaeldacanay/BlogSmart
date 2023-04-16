20334732de14ba7099368380fe45e3c5e9842650
michaeldacanay <michaeldacanayusa@gmail.com>
2023-04-16 06:58:19 -0700

    Add Sign Up view

---
+++ BlogSmart/BlogSmart/SignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignUpViewController.swift
+//  BlogSmart
+//
+//  Created by Michael Dacanay on 4/16/23.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
