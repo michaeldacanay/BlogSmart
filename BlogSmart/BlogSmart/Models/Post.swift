7fbdaa1060fcd531b2ddf610315125516c2e78a2
Raunaq Malhotra <raunaqmalhotra02@gmail.com>
2023-04-27 00:42:05 -0400

    add custom blog content property to the Post struct

--- BlogSmart/BlogSmart/Models/Post.swift
+++ BlogSmart/BlogSmart/Models/Post.swift
@@ -19,6 +19,7 @@ struct Post: ParseObject {
     
     // Custom properties for the app
     var title: String?
+    var content: String?
     var summary: String?
     var user: User?
     var imageFile: ParseFile?
