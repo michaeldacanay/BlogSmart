dddcf6ca4e872d362d4c4af5e3c28c5893d32e05
michaeldacanay <michaeldacanayusa@gmail.com>
2023-04-27 20:20:39 -0700

    Add URL request code

--- BlogSmart/BlogSmart/Write/WriteViewController.swift
+++ BlogSmart/BlogSmart/Write/WriteViewController.swift
@@ -16,6 +16,47 @@ class WriteViewController: UIViewController {
     @IBOutlet weak var blogContent: UITextField!
     
     private var pickedImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // Create a URL for the request
+        // In this case, the custom search URL you created in in part 1
+        let url = URL(string: "https://api.openai.com/v1/chat/completions")!
+
+        // Use the URL to instantiate a request
+        let request = URLRequest(url: url)
+
+        // Create a URLSession using a shared instance and call its dataTask method
+        // The data task method attempts to retrieve the contents of a URL based on the specified URL.
+        // When finished, it calls it's completion handler (closure) passing in optional values for data (the data we want to fetch), response (info about the response like status code) and error (if the request was unsuccessful)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+
+            // Handle any errors
+            if let error = error {
+                print("❌ Network error: \(error.localizedDescription)")
+            }
+
+            // Make sure we have data
+            guard let data = data else {
+                print("❌ Data is nil")
+                return
+            }
+
+            // The `JSONSerialization.jsonObject(with: data)` method is a "throwing" function (meaning it can throw an error) so we wrap it in a `do` `catch`
+            // We cast the resultant returned object to a dictionary with a `String` key, `Any` value pair.
+            do {
+                let jsonDictionary = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+                print(jsonDictionary)
+            } catch {
+                print("❌ Error parsing JSON: \(error.localizedDescription)")
+            }
+        }
+
+        // Initiate the network request
+        task.resume()
+    }
 
     
     @IBAction func onChooseImageTapped(_ sender: Any) {
@@ -116,11 +157,7 @@ class WriteViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
     
 
 }
