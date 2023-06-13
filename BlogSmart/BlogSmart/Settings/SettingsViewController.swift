b738382325f77129b6a94ec5a1967af6d4ccfe0b
michaeldacanay <michaeldacanayusa@gmail.com>
2023-06-13 19:38:51 -0400

    Add settings save updates function, email format validation, saving changes to Parse backend

--- BlogSmart/BlogSmart/Settings/SettingsViewController.swift
+++ BlogSmart/BlogSmart/Settings/SettingsViewController.swift
@@ -6,24 +6,78 @@
 //
 
 import UIKit
+import ParseSwift
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let user = User.current
+        username.placeholder = user?.username
+        email.placeholder = user?.email
+        password.placeholder = user?.password
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onSave(_ sender: Any) {
+        // Retrieve the object from the database
+        if var currentUser = User.current {
+            if !email.text!.isEmpty {
+                if !isValidEmail(email.text!) {
+                    showInvalidEmailAlert()
+                    return
+                }
+                currentUser.email = email.text
+            }
+            
+            if !username.text!.isEmpty {
+                // Update the field
+                currentUser.username = username.text
+            }
+            
+            if !password.text!.isEmpty {
+                currentUser.password = password.text
+            }
+            
+            // Save the changes
+            currentUser.save { result in
+                switch result {
+                case .success(let updatedUser):
+                    print("Field updated successfully: \(updatedUser)")
+                case .failure(let error):
+                    print("Error updating field: \(error)")
+                }
+            }
+        } else {
+            print("No current user")
+        }
+        
+        if !email.text!.isEmpty || !username.text!.isEmpty || !password.text!.isEmpty {
+            // return to ReadViewController
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        
     }
-    */
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+    
+    private func showInvalidEmailAlert() {
+        let alertController = UIAlertController(title: "Opps...", message: "The email provided is invalid.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+
+    
 
 }
