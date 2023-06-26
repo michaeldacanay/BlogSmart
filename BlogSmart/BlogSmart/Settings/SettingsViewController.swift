802487492f82efb7b9209c8b1e38c9614603f6c5
Raunaq Malhotra <raunaqmalhotra02@gmail.com>
2023-06-26 19:05:19 -0400

    delete posts associated to the account and the account

--- BlogSmart/BlogSmart/Settings/SettingsViewController.swift
+++ BlogSmart/BlogSmart/Settings/SettingsViewController.swift
@@ -65,6 +65,71 @@ class SettingsViewController: UIViewController {
         
     }
     
+    
+    @IBAction func onDeleteClicked(_ sender: Any) {
+        showConfirmDelete()
+    }
+    
+    private func showConfirmDelete() {
+        let alertController = UIAlertController(title: "Delete your account?", message: "This action will delete your account permanently and all the posts that you have created. You will not be able to restore your account or your posts. Are you sure?", preferredStyle: .alert)
+        
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            
+            if var currentUser = User.current {
+                
+                let query = Query<Post>()
+                
+                query.whereKey("user", equalTo(key: currentUser)) // "user" is the pointer column to the User class
+//                let query = Query<Post>.query("user", .equalTo(currentUser))
+                
+                query?.find { result in
+                    switch result {
+                    case .success(let posts):
+                        // Posts associated with the user found
+                        self.deletePosts(posts) // Proceed with deleting the posts
+                    case .failure(let error):
+                        // Handle the error retrieving posts
+                        print("Error retrieving posts: \(error)")
+                    }
+                }
+
+                currentUser.delete { result in
+                    switch result {
+                    case .success:
+                        // Account deletion successful
+                        print("Account deleted successfully")
+                        
+                    case .failure(let error):
+                        // Handle the error that occurred during account deletion
+                        print("Error deleting account: \(error)")
+                    }
+                }
+            }
+            
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(deleteAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    func deletePosts(_ posts: [Post]) {
+        for post in posts {
+            post.delete {
+                result in switch result {
+                case .success:
+                    // Post deletion successful
+                    print("Post deleted successfully")
+                case .failure(let error):
+                    // Handle the error that occurred during post deletion
+                    print("Error deleting post: \(error)")
+                }
+            }
+        }
+    }
+    
     func isValidEmail(_ email: String) -> Bool {
         let emailRegex = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
